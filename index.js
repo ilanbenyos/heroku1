f849c99f972dddba0aaaff50b17050447febac76
ilanbenyos <ilan_ben@hotmal.com>
2019-05-27 10:42:46 +0300

    init koala

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+require('babel-core/register')()
+require('babel-polyfill')
+require('./server.js')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
